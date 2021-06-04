75ca8cdde41aa2f9fbae67f57009b8e60cf912bd
unknown <amalbabu1200@gmail.com>
2021-06-04 08:52:37 +0530

    login page committed-2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import './App.css';
-import { Route , Switch } from "react-router-dom";
-import Login from './components/Login';
-import Layout from './components/Layout';
-
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <div className="app">
       <Switch>
-        <Route path='/' exact> <Login /> </Route>
-        <Route path='/layout'><Layout/></Route>
+        <Route path="/" exact>
+          {" "}
+          <Login />{" "}
+        </Route>
+        <Route path="/layout">
+          <Layout />
+        </Route>
       </Switch>
-      
     </div>
   );
 }
