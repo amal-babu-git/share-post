6c773e4e06f454ec1b85b8a5028e519e89775337
Amal Babu <amalbabu1200@gmail.com>
2022-06-25 09:56:28 +0530

    rm fb

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,7 @@ import firebase from "firebase";
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBlj5V9BZ6IyU_6sFeqUZtENpos-5UQaFM",
-    authDomain: "share-post-app.firebaseapp.com",
-    projectId: "share-post-app",
-    storageBucket: "share-post-app.appspot.com",
-    messagingSenderId: "236461700447",
-    appId: "1:236461700447:web:ce5bdb0373cb37656af2ea"
+    
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
